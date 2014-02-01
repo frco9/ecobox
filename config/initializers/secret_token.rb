88b76c00650d38c550098729163d40e09d79e7cb
Jérémie Foucault <Jeremie@MacBook-Pro-de-Jeremie.local>
2014-02-01 15:11:04 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecobox::Application.config.secret_key_base = '8d3512f34ce761b4acd4c75adec3f6f60116888e0b6ae48d4bc638f98a5dd05befef46782c3d7254495eec7e72a4cccf26336afdde9ba8a5b73823e29cb4c8cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ecobox::Application.config.session_store :cookie_store, key: '_ecobox_session'
