aabcc6e135fd63f2816f37a2ea9a16558e937b69
idyr <syd@ubuntu.(none)>
2014-02-12 19:39:05 +0100

    Ajout du controlleur Home

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+class HomeController < ApplicationController
+  
+# Fonction Principale de la page Home
+  def index
+# Recuperation de toutes les pieces:
+    @rooms = Room.all
+ 
+# Recuperation Des capteurs de types 
+    @data_types = DataType.where(["name = 'temperature'"])
+    tmp = Time.now
+#    time_of_availability = Time.new(tmp.year,tmp.month,tmp.day,tmp.hour,tmp.min- 2,tmp.sec)
+    @last_data = DataSensor.last
+    if @last_data != nil
+       @current_temperature s = @last_data.where("data_type = ?",@data_types)
+    end
+
+    if @current_temperatures != nil
+        @current_temp = 0.0
+        nb_data = 0.0
+        @current_temperatures.each do |data_sensor|
+           @current_temp += data_sensor.value
+           nb_data += 1.0
+        end
+        @current_temp = @current_temp/nb_data
+     else 
+        @current_temp = 9999
+    end
+  end
+end
