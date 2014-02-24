f9e0c2abf35ea880e620f51f81be2f817a136b32
idyr <syd@ubuntu.(none)>
2014-02-24 13:04:59 +0100

    Avancement de la page home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,40 +1,39 @@
 # -*- coding: utf-8 -*-
 class HomeController < ApplicationController
-  
+  include HomeHelper
 # Main fonction of the Home page
   def index
 
 # Getting of all rooms:
     @rooms = Room.all
  
-<<<<<<< HEAD
 # Getting of the last value for each sensors: 
 # => pb pour capteur a plusieurs types de donnees
     @sensors = Sensor.all
-    @lastdatas = []
-        
-    @sensors.each do |sensor|    
-        @lastdatas << sensor.data_sensors.last
-=======
-# Recuperation Des capteurs de types 
-    @data_types = DataType.where(["name = 'Temperature'"])
-    tmp = Time.now
-#    time_of_availability = Time.new(tmp.year,tmp.month,tmp.day,tmp.hour,tmp.min- 2,tmp.sec)
-    @last_data = DataSensor.last
-    if @last_data != nil
-       #@current_temperatures = @last_data.where("data_type = ?",@data_types)
->>>>>>> f85e53b011dc229623739edb6a3eba0ead941e84
-    end
-# Getting of types:
 
+
+
+# Getting of types:
     @temp_type = DataType.find_by_name('Temperature')
     @press_type = DataType.find_by_name('Pression')
 
-# Getting of values 
+    
+# Getting of the daily datas:      
+    @dailydatas = []
+    @sensors.each do |sensor|
+          datas = sensor.data_sensors
+          datas.each do |data_sensor|   
+              if data_sensor.created_at.beginning_of_day == Time.now.beginning_of_day
+                  @dailydatas << sensor.data_sensors
+              end
+          end
+    end
+
+# Separation of datas by type: 
     @temperature_datas = []
     @pression_datas = []
     
-    @lastdatas.each do |data_sensor|
+    @dailydatas.each do |data_sensor|
        if data_sensor.data_type == @temp_type
            @temperature_datas << data_sensor 
        else 
@@ -45,19 +44,32 @@ class HomeController < ApplicationController
     end 
    
 
-    if !@temperature_datas.empty?
-        @current_temp = 0.0
-        nb_data = 0.0
-        @temperature_datas.each do |data_sensor|
-           #if data_sensor.created_at >= time_of_availability
-           @current_temp += data_sensor.value
-           nb_data += 1.0
-           #end
-        end
-        @current_temp = @current_temp/nb_data
-    else 
-        @current_temp = 9999
-    logger.debug @current_temp
-    end
+# Getting daily temperature statistics:
+    @min_temp = min(@temperature_datas)
+    @max_temp = max(@temperature_datas)
+    @avg_temp = average(@temperature_datas)
+
+
+# Getting unavailable sensors:
+
+    @unavailable_sensors = unavailable_sensors
+   
+
+    # if !@temperature_datas.empty?
+    #     @current_temp = 0.0
+    #     nb_data = 0.0
+    #     @temperature_datas.each do |data_sensor|
+    #        #if data_sensor.created_at >= time_of_availability
+    #        @current_temp += data_sensor.value
+    #        nb_data += 1.0
+    #        #end
+    #     end
+    #     @current_temp = @current_temp/nb_data
+    # else 
+    #     @current_temp = 9999
+    # logger.debug @current_temp
+    # end
+
+
   end       
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,32 +1,58 @@
 module HomeHelper
 
       def max(tab)
-            maximum = tab[0]
-            tab.each do |float|
-                  if float > maximum
-                      maximum = float
-                  end
+            if !tab.empty?
+               maximum = tab[0]
+               tab.each do |float|
+                     if float > maximum
+                        maximum = float
+                    end
+               end
+               return maximum
+            else
+               return "Unavailable"
             end
-            return maximum
       end
  
       def min(tab)
-            minimum = tab[0]
-            tab.each do |float|
-                  if float < minimum
-                      minimum = float
-                  end
-            end
-            return minimum
+           if !tab.empty?
+              minimum = tab[0]
+              tab.each do |float|
+                    if float < minimum
+                        minimum = float
+                    end
+              end
+              return minimum
+           else
+              return "Unavailable"
+           end
       end
 
       def average(tab)
-            sum = 0
-            tab.each do |float|
-                 sum += float
+            if !tab.empty?
+               sum = 0
+               tab.each do |float|
+                    sum += float
+               end
+               avg = sum/tab.length
+               return avg
+            else
+               return 9999
             end
-            avg = sum/tab.size
-            return avg
+      end
+
+      def unavailable_sensors
+            sensors_tab = []
+            tmp = Time.now
+            time_of_availability = Time.new(tmp.year,tmp.month,tmp.day,tmp.hour,tmp.min - 2,tmp.sec)
+            
+            @sensors.each do |sensor|
+                 datas = sensor.data_sensors.last
+                 if datas.created_at < time_of_availability
+                     sensors_tab << sensor
+                 end
+            end
+            return sensors_tab
       end
 
 end
