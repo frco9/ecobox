1a094b41bb1cfc1d5effa89a618554effa499676
Jérémie Foucault <Jeremie@MacBook-Pro-de-Jeremie.local>
2014-03-30 19:19:56 +0200

    Try to run in production mode failed/ just remove debug message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,9 @@ Ecobox::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
